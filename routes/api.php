2c0b854f7e987fbe5d5160016457b72a000b4f7b
ChoirulArdhi <choirulardhi2@gmail.com>
2025-11-11 09:31:51 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::resource('products', ProductController::class);
+    
+    Route::resource('vendors', VendorController::class);
 
 Route::get('/halo', function () {
     return 'halo Laravel';
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+});
+
